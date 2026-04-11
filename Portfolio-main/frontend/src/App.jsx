ec1f4756db82f6d87305c4e3db36206bd07ec848
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-04-11 13:59:30 +0530

    update

--- Portfolio-main/frontend/src/App.jsx
+++ Portfolio-main/frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import profile from '../assets/profile.jpeg';
-import Testimonials from '../components/testimonials.jsx';
-import RotatingText from '../components/text.jsx';
+import profile from './assets/profile.jpeg';
+import Testimonials from './components/testimonials.jsx';
+import RotatingText from './components/text.jsx';
 
 const Home = () => {
   const navigate = useNavigate();
